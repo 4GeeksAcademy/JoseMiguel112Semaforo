cfb0e1f83d505d72c70fb0fd734cd11dc6aeeeb7
Jose <jm.bebor@gmail.com>
2025-06-13 10:59:58 +0000

    [Not finished]must build logic

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import '../../styles/index.css'
+
 //create your first component
 const Home = () => {
+
+
+
 	return (
 		<div className="text-center">
-            
+			<div className="justify-content-center" style={{ width: "200px" }}>
+				<div className="d-flex justify-content-center">
+					<div className="bg-dark mx-4" style={{ width: "20px", height: "100px" }} >	</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+				</div>
+				<div className="d-flex justify-content-center">
+
+					<div className="bg-dark rounded" style={{ width: "100px", height: "300px"}}>
+						<button className="btn bg- rounded-circle mt-3" style={{width:"85px", height: "85px", boxshadow: "red 60px -16px"}}></button>
+						<button className="btn bg-warning rounded-circle mt-2" style={{width:"85px", height: "85px"}}></button>
+						<button className="btn bg-success rounded-circle mt-2" style={{width:"85px", height: "85px"}}></button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
