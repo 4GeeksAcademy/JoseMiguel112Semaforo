c21b2e5815d5f6e1f8681035f417793ccee2d4e6
Jose <jm.bebor@gmail.com>
2025-06-15 17:46:52 +0000

    [FinishedBut]Horrendous code

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,23 +7,45 @@ import '../../styles/index.css'
 
 //create your first component
 const Home = () => {
+	
+	const [redlight, setRedLight] = useState("btn bg-danger rounded-circle mt-3");
+	const [orangelight, setOrangeLight] = useState("btn bg-warning rounded-circle mt-2");
+	const [greenlight, setGreenLight] = useState("btn bg-success rounded-circle mt-2");
 
+	function turnOnRedLight(){
+		setRedLight("btn bg-danger rounded-circle mt-3 light")
+		setOrangeLight("btn bg-warning rounded-circle mt-2")
+		setGreenLight("btn bg-success rounded-circle mt-2")
+		console.log(redlight);
+		
+	}
 
+	function turnOnOrangeLight(){
+		setRedLight("btn bg-danger rounded-circle mt-3")
+		setOrangeLight("btn bg-warning rounded-circle mt-2 light")
+		setGreenLight("btn bg-success rounded-circle mt-2")
+	}
+
+	function turnOnGreenLight(){
+		setRedLight("btn bg-danger rounded-circle mt-3")
+		setOrangeLight("btn bg-warning rounded-circle mt-2")
+		setGreenLight("btn bg-success rounded-circle mt-2 light")
+	}
 
 	return (
 		<div className="text-center">
 			<div className="justify-content-center" style={{ width: "200px" }}>
 				<div className="d-flex justify-content-center">
-					<div className="bg-dark mx-4" style={{ width: "20px", height: "100px" }} >	</div>
+					<div className="bg-dark mx-4" style={{ width: "20px", height: "50px" }} >	</div>
 
 
 				</div>
 				<div className="d-flex justify-content-center">
 
 					<div className="bg-dark rounded" style={{ width: "100px", height: "300px"}}>
-						<button className="btn bg- rounded-circle mt-3" style={{width:"85px", height: "85px", boxshadow: "red 60px -16px"}}></button>
-						<button className="btn bg-warning rounded-circle mt-2" style={{width:"85px", height: "85px"}}></button>
-						<button className="btn bg-success rounded-circle mt-2" style={{width:"85px", height: "85px"}}></button>
+						<button className={redlight} style={{width:"85px", height: "85px"}} onClick={turnOnRedLight}></button>
+						<button className={orangelight} style={{width:"85px", height: "85px"}} onClick={turnOnOrangeLight}></button>
+						<button className={greenlight} style={{width:"85px", height: "85px"}} onClick={turnOnGreenLight}></button>
 					</div>
 				</div>
 			</div>
